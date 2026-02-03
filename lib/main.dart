dc24f79c1b43408ab3633e55aad9743883cd8f10
Baber19 <Baber19i@outlook.com>
2026-02-03 10:53:03 +0500

    ListView Builder Practice

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class ListViewBuilderApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("data"),
+        child: Text("ListViewBuilderApp"),
       ),
     );
   }
