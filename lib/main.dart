3130a5049525e51ad6550db7fb3d21c91a643f9d
Baber19 <Baber19i@outlook.com>
2026-02-03 10:52:42 +0500

    ListView Builder Practice

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,10 @@ class ListViewBuilderApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text("data"),
+      ),
+    );
   }
 }
